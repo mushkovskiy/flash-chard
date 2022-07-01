53b5047012ee44f5f94f0bc18da2312832e78067
Elena <elli_shii@mail.ru>
2022-07-01 11:51:10 +0300

    add className)

--- views/Card.jsx
+++ views/Card.jsx
@@ -5,13 +5,13 @@ const Layout = require('./Layout');
 module.exports = function Card({ theme }) {
   return (
     <Layout title={title}>
-      {/* <div class="card text-center border-info mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;"> */}
-      <div class="card text-center border-warning mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">{theme.title}</h5>
-          {/* <p class="card-text">С вспомогательным текстом ниже в качестве естественного перехода к дополнительному контенту.</p> */}
-          {/* <a href={`/theme/${theme.id}`} class="btn btn-primary">Выбрать тему</a> */}
-          <a href={`/theme/${theme.id}`} class="btn btn-outline-dark">Выбрать тему</a>
+      {/* <div className="card text-center border-info mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;"> */}
+      <div className="card text-center border-warning mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
+        <div className="card-body">
+          <h5 className="card-title">{theme.title}</h5>
+          {/* <p className="card-text">С вспомогательным текстом ниже в качестве естественного перехода к дополнительному контенту.</p> */}
+          {/* <a href={`/theme/${theme.id}`} className="btn btn-primary">Выбрать тему</a> */}
+          <a href={`/theme/${theme.id}`} className="btn btn-outline-dark">Выбрать тему</a>
         </div>
       </div>
     </Layout>
